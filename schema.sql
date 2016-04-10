8305b1189e81ec77eb7ace4f3aae53c8ccab331d
AltF4 <altf4@phx2600.org>
2016-04-10 15:53:55 -0700

    Closer to registration working

--- schema.sql
+++ schema.sql
@@ -1,11 +1,10 @@
 -- To reload the tables:
 --   sqlite3 database.db < schema.sql
 
-DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id INTEGER PRIMARY KEY NOT NULL,
     name TEXT(20) NOT NULL UNIQUE,
-    score REAL(10, 2),
     hash TEXT(100) NOT NULL
 );
 
@@ -18,6 +17,5 @@ CREATE TABLE flags (
 --TODO replace in prod
 INSERT into flags (value) VALUES ('congrats flag1');
 
-INSERT into teams (name, score, hash) VALUES ('aaa', 0, '$2a$04$v6PKN3tpOyaiKV/3VZOjh.RRoDUDvITVLZuSwhzyRVbK82ANFMQOi');
-INSERT into teams (name, score, hash) VALUES ('bbb', 0, '$2a$12$ydNsdi763MvDytMGdiBNE.rqWeoJxx9pRYHKyIRZ3l/E.x6pLOLmi');
-INSERT into teams (name, score, hash) VALUES ('ccc', 0, '$2a$12$uW7DWD3n497ZlVA1gJiuhOftfIudF/nINoiQKwm2/3rnvjuCg6Ldy');
+--TODO put in admin user
+-- INSERT into users (user, hash) VALUES ('admin', '$2a$12$uW7DWD3n497ZlVA1gJiuhOftfIudF/nINoiQKwm2/3rnvjuCg6Ldy');
--- src/obedientart.py
+++ src/obedientart.py
@@ -2,8 +2,10 @@ import tornado.ioloop
 import tornado.web
 import os
 import sqlite3
+import bcrypt
 
 app_dir = os.path.dirname(os.path.realpath(__file__))
+db = sqlite3.connect(app_dir + '/../database.db')
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -20,9 +22,55 @@ class LoginHandler(BaseHandler):
     def get(self):
         self.render(self.render(app_dir + "/public/login.html"))
 
+    def post(self):
+        user = self.get_argument('user', None)
+        password = self.get_argument('password', None)
+        if not user or not password:
+            raise tornado.web.HTTPError(403)
+
+        global db
+        cursor = db.cursor()
+        packaged = (user, ) #no idea why you have to do this
+        cursor.execute("SELECT * from users WHERE name=? LIMIT 1", packaged)
+        row = cursor.fetchone()
+        if not row:
+            raise tornado.web.HTTPError(403)
+
+        username = row[1]
+        passhash = row[2]
+
+        password = password.encode('utf-8')
+        if passhash == bcrypt.hashpw(password, passhash.encode('utf-8')) and teamname == team:
+            self.set_secure_cookie("userid", teamname, httponly=True, expires_days=1)
+        else:
+            raise tornado.web.HTTPError(403)
+
 class RegistrationHandler(BaseHandler):
     def get(self):
         self.render(self.render(app_dir + "/public/register.html"))
+
+    def post(self):
+        user = self.get_argument('user', None)
+        password = self.get_argument('password1', None)
+        if not user or not password:
+            raise tornado.web.HTTPError(403)
+
+        global db
+        cursor = db.cursor()
+        packaged1 = (user, ) #no idea why you have to do this
+
+        #Does the user already exist?
+        cursor.execute("SELECT * from users WHERE name=? LIMIT 1", packaged1)
+        row = cursor.fetchone()
+        if row:
+            raise tornado.web.HTTPError(402)
+
+        password = password.encode('utf-8')
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        packaged2 = (user, hashed, ) #no idea why you have to do this
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO users (name, hash) VALUES (?, ?)", packaged2)
+
 
 def make_app():
     return tornado.web.Application([
